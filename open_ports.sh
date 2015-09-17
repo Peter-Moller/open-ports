e76a176e0a243006402e952ce8ec49bf00ea89d8
Peter Möller <peter.moller@cs.lth.se>
2015-09-17 08:48:58 +0200

    Apache License v 2.0 added
    Added license and edited the comments in the head accordingly

--- open_ports.sh
+++ open_ports.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 #
-# Script to list open TCP-connections on Mac OS X 10.6, 10.7, 10.8 and 10.9 and Linux
-# © Peter Möller, 2011-2014
-# 2011-04-04 / Peter Möller, Dept of Copmuter Science, Lund University
+# Script to list open TCP-connections on Mac OS X 10.6, 10.7, 10.8, 10.9, 10.10 and 10.11 and Linux
+# The script came to life in 2011
+# 
+# Copyright 2015 Peter Möller, Dept of Copmuter Science, Lund University
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# 
 # Script home page (english):
 # http://cs.lth.se/kontakt/peter_moller/script/open_portssh_en
 # Script home page (swedish):
